34dc9d563f180d48e31dbdd8d084c3c873ebd4c7
Mark Huot <mark@markhuot.com>
2023-10-29 08:31:55 -0400

    Update SlotDefinition.php

--- src/base/SlotDefinition.php
+++ src/base/SlotDefinition.php
@@ -92,7 +92,7 @@ class SlotDefinition
 
     public function render(array $context=[]): Markup
     {
-        return new \Twig\Markup($this->component->getSlot($this->name)->render($context), 'utf-8');
+        return $this->component->getSlot($this->name)->render($context);
     }
 
     public function __toString(): string
