82a0d7cda89adecd9afcb02c20b3bdb900a40920
markhuot <mark@markhuot.com>
2023-10-31 03:06:56 -0400

    dead code cleanup

--- tests/EagerLoadsComponentsTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use markhuot\keystone\models\Component;
-
-it('eager loads all components for an element/field combo', function () {
-    $section = Component::factory()->create(['type' => 'keystone/section']);
-    Component::factory()->create(['type' => 'keystone/text']);
-})->skip();
