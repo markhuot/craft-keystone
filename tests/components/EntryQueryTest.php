923fbec18ce31fa1f0b8c90930cb861d0d3c76aa
markhuot <mark@markhuot.com>
2023-11-28 21:31:38 -0500

    entry queries and assets all use templates now

--- tests/components/EntryQueryTest.php
+++ tests/components/EntryQueryTest.php
@@ -5,9 +5,9 @@ use markhuot\keystone\models\Component;
 
 it('renders element queries', function () {
     $entry = Entry::factory()->title('foobarbaz')->create();
-    $component = Component::factory()->type('keystone/elementquery')->create();
+    $component = Component::factory()->type('keystone/entryquery')->create();
     $test = Component::factory()->type('keystone/text')->path($component->id)->create();
-    $test->data->merge(['text' => '{element.title}'])->save();
+    $test->data->merge(['text' => '{entry.title}'])->save();
     $response = $component->render();
     expect($response)->toContain('foobarbaz');
 });
